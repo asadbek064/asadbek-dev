7499e3a3f3b48e1a7b49bec2171e699add396b7e
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-05-22 15:11:04 -0400

    project contrast bg

--- app/project/project-list.tsx
+++ app/project/project-list.tsx
@@ -6,17 +6,17 @@ const ProjectList = ({ projects }) => {
       {projects.map((project) => (
         <div
           key={project.id}
-          className="p-6 bg-neutral-900 rounded-lg shadow-md"
+          className="p-6 bg-neutral-800 rounded-lg shadow-md"
         >
           <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-          <p className="text-gray-400 mb-4">{project.description}</p>
+          <p className="text-gray-300 mb-4">{project.description}</p>
           
 
           <a
             href={project.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-block bg-neutral-200 hover:bg-neutral-700 hover:text-neutral-200 text-neutral-700 font-semibold py-1 px-3 rounded-md"
+            className="inline-block bg-neutral-200 hover:bg-neutral-700 hover:text-neutral-200 text-neutral-900 font-semibold py-1 px-3 rounded-md"
           >
             View Project
           </a>
