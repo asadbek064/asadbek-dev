828b5109e244dcd4ece02ba3aa3ee4ef144148c8
Asad <asadbekasadbek@gmail.com>
2023-08-31 19:13:05 -0400

    resume update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,7 +40,7 @@ export default function AboutPage() {
           <a
             rel="noopener noreferrer"
             target="_blank"
-            href="/Asadbek_Karimov_Resume.pdf"
+            href="/Asadbek-Karimov-Resume-2023.pdf"
             className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
           >
             <div className="flex items-center">
