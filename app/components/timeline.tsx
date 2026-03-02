6112564d052f721a1ca500d57d22e8e5a6e02fb9
Asad <contact@asadk.dev>
2026-03-02 13:07:26 -0500

    chore: fixed timelines layout

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -40,23 +40,23 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         style={{ position: "relative" }}
         ref={containerRef}
       >
-        <div className="relative max-w-7xl mx-auto pb-20 md:px-8">
+        <div ref={ref} className="relative mx-auto pb-20 px-4 md:px-8">
           {data.map((item, index) => (
-            <div key={index} className="relative pt-10 md:pt-20">
+            <div key={index} className="relative pt-10 md:pt-16">
               {/* Timeline dot */}
-              <div className="absolute left-3 md:left-8 top-10 md:top-20 h-10 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center z-10">
-                <div className="h-4 w-4 rounded-full bg-neutral-300 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+              <div className="absolute left-4 md:left-8 top-10 md:top-16 h-8 w-8 rounded-full bg-white dark:bg-black flex items-center justify-center z-10">
+                <div className="h-3 w-3 rounded-full bg-neutral-300 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700" />
               </div>
 
               {/* Title */}
-              <div className="relative pl-20 pr-4 md:pl-24 md:pr-8">
-                <h2 className="text-xl md:text-3xl lg:text-4xl font-bold text-neutral-500 dark:text-white mb-6">
+              <div className="pl-16 md:pl-20 pr-2 md:pr-4">
+                <h2 className="text-xl md:text-3xl lg:text-4xl font-bold text-neutral-500 dark:text-white mb-4">
                   {item.title}
                 </h2>
               </div>
 
               {/* Content */}
-              <div className="relative pl-20 pr-4 md:pl-24 md:pr-8 w-full">
+              <div className="pl-16 md:pl-20 pr-2 md:pr-4 w-full">
                 {item.content}
               </div>
             </div>
@@ -67,7 +67,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             style={{
               height: height + "px",
             }}
-            className="absolute left-8 md:left-13 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
+            className="absolute left-12 md:left-20 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
           >
             <motion.div
               style={{
